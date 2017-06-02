55fdfa2897b0df7e54f1838c1c75243c7ed27602
Use鄭瑜 <zhyu@cere.ntnu.edu.tw>
2017-06-02 10:45:14 +0800

    plat-756: user not in no population list will be created and login

--- app/controllers/SurveyController.php
+++ app/controllers/SurveyController.php
@@ -71,7 +71,14 @@ class SurveyController extends \BaseController {
         $in_rows  = DB::table('rows.dbo.'.$table->name)->where('C'.$file_book->loginRow_id, $login_id)->exists();
 
         if (!$in_rows) {
-             return Redirect::to('survey/'.$book_id.'/survey/surveyLogin');
+            if ($file_book->no_population) {
+                $user_id = Files::find($file_book->rowsFile_id)->created_by;
+                $current_time = Carbon\Carbon::now()->toDateTimeString();
+                $query = DB::table($table->database . '.dbo.' . $table->name);
+                $query->insert(['C'.$file_book->loginRow_id => $login_id, 'file_id' => 1, 'updated_at' => $current_time, 'created_at' => $current_time, 'updated_by' => $user_id, 'created_by' => $user_id]);
+            } else {
+                return Redirect::to('survey/'.$book_id.'/survey/surveyLogin');
+            }
         }
 
         $encrypt_id = SurveySession::login($book_id, $login_id);
