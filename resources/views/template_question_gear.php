93fbd57fce74bc77cbb8a026043126d9ba5f6d50
tim <tim72117@gmail.com>
2018-01-12 13:27:17 +0800

    PLAT-1336: fix gear template

--- resources/views/template_question_gear.php
+++ resources/views/template_question_gear.php
@@ -1,7 +1,7 @@
 <div>
     <div ng-repeat="question in node.questions">
         <label>{{question.title}}</label>
-        <md-select ng-model="answers[question.name]" ng-disabled="node.saving" survey-input parent="answers[question.name]" placeholder="請選擇" ng-change="saveAnswer(answers[question.id])">
+        <md-select ng-model="answers[question.id]" ng-disabled="node.saving" survey-input placeholder="請選擇" ng-change="saveAnswer(answers[question.id])">
             <md-option ng-repeat="answer in node.answers" ng-value="answer.value" ng-disabled="skips.answers.indexOf(answer.id) != -1">{{answer.title}}</md-option>
         </md-select>
         <survey-node ng-repeat="children in question.childrens" node="children" ng-hide="skips.nodes.indexOf(node.id) != -1"></survey-node>
