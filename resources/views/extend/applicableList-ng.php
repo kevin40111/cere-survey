54fc7b359c25cafb2e89102050a9f8224d3821c2
Kevin <a097245@gmail.com>
2018-01-30 18:52:48 +0800

    PLAT-1360: use factory to save fields

--- resources/views/extend/applicableList-ng.php
+++ resources/views/extend/applicableList-ng.php
@@ -14,16 +14,16 @@
                             <div class="header">請先完成登入設定</div>
                         </div>
                     </md-list-item>
-                    <md-list-item ng-repeat="column in applicable.options.columns">
+                    <md-list-item ng-repeat="column in columns">
                         <p>{{column.title}}</p>
-                        <md-checkbox class="md-secondary" ng-checked="exists(column, columnselected)" ng-click="toggle(column, columnselected, $event)" ng-true-value="true" ng-false-value="" aria-label="{{column.title}}"></md-checkbox>
+                        <md-checkbox class="md-secondary" ng-checked="column.selected" ng-click="toggle(column)"></md-checkbox>
                     </md-list-item>
                     <md-divider ></md-divider>
                     <md-subheader class="md-no-sticky" md-colors="{color: 'indigo-800'}"><h4>可申請題目數量限制</h4></md-subheader>
                     <md-list-item>
                         <md-input-container>
                             <label>題目數量限制</label>
-                            <input type="number" ng-model="applicable.extend.rule.fieldsLimit" />
+                            <input type="number" ng-model="fieldsLimit" />
                         </md-input-container>
                     </md-list-item>
                     <md-divider ></md-divider>
@@ -33,19 +33,7 @@
                         <button class="ui blue button" flex= 30 ng-click="showQuestion($event)">新增題目</button>
                     </md-list-item>
                     <md-divider ></md-divider>
-                    <md-subheader class="md-no-sticky" md-colors="{color: 'red'}">共釋出{{selected_length}}個欄位</md-subheader>
-                    <div style="border:1px solid grey; margin:20px;">
-                        <p>105年度高二調查問卷</p>
-                        <div>
-                            <md-select ng-model="pages">
-                                <md-option ng-repeat="page in haveSelect" ng-value="page">{{$index+1}}</md-option>
-                            </md-select>
-                            <div ng-repeat="question_column in pages">
-                                {{question_column.title}}
-                                <md-checkbox ng-click="toggle(question_column)" ng-checked="question_column.selected" aria-label="question_column">
-                            </div>
-                        </div>
-                    </div>
+                    <md-subheader class="md-no-sticky" md-colors="{color: 'red'}">共釋出{{selected.length}}個欄位(含母體)</md-subheader>
                 </md-list>
             </md-content>
         </md-card>
@@ -53,46 +41,64 @@
     </div>
 </md-content>
 <script>
-    app.controller('application', function ($scope, $http, $filter, $mdDialog){
-        $scope.applicable = {};
-        $scope.disabled = false;
+    app.factory('fieldsFactory', function() {
+        this.selected = [];
+        this.columns = [];
+        this.questions = [];
+
+        return {
+            columns: this.columns,
+            questions: this.questions,
+            selected: this.selected,
+            toggle: function (item) {
+                item.selected = !item.selected;
+                var idx = this.selected.indexOf(item.id);
+                if(idx>-1){
+                    this.selected.splice(idx, 1);
+                }else {
+                    this.selected.push(item.id);
+                }
+                console.log(this.selected);
+                return this.selected;
+            }
+        }
+    });
+
+    app.controller('application', function ($scope, $http, $filter, $mdDialog, fieldsFactory){
         $scope.empty = false;
-        $scope.columnselected =[];
+
+        $scope.toggle = function (column) {
+            fieldsFactory.toggle(column);
+        }
 
         $scope.getApplicableOptions = function() {
             $http({method: 'POST', url: 'getApplicableOptions', data:{}})
             .success(function(data, status, headers, config) {
-                angular.extend($scope.applicable, data);
+                fieldsFactory.selected = data.extend.rule.fields;
+                fieldsFactory.columns = data.options.columns;
+                fieldsFactory.questions = data.options.questions;
+
+                $scope.selected = fieldsFactory.selected;
+                $scope.columns = fieldsFactory.columns;
+                $scope.fieldsLimit = data.extend.rule.fieldsLimit;
+                $scope.columnsLimit = data.extend.rule.columnsLimit;
+                $scope.conditionColumn_id = data.extend.rule.conditionColumn_id
             })
             .error(function(e){
                 console.log(e);
             });
         }
-
-        function getFields() {
-            var fields = {};
-            $filter('filter')($scope.applicable.options.columns, {selected: true}).forEach(function(field) {
-                fields[field.id] = {target: 'login'};
-            });
-            $filter('filter')($scope.applicable.options.questions, {selected: true}).forEach(function(field) {
-                fields[field.id] = {target: 'main'};
-            });
-
-            return fields;
-        }
+        $scope.getApplicableOptions();
 
         $scope.setApplicableOptions = function() {
-            // var fields = getFields();
-            var fields = [];
-
-            if ($scope.applicable.extend.rule.conditionColumn_id) {
+            if ($scope.conditionColumn_id) {
                 $scope.disabled = true;
                 $http({method: 'POST', url: 'setApplicableOptions', data:{
                     selected: {
-                        'fieldsLimit': $scope.applicable.extend.rule.fieldsLimit,
-                        'columnsLimit' : $scope.applicable.extend.rule.columnsLimit,
-                        'fields': fields,
-                        'conditionColumn_id': $scope.applicable.extend.rule.conditionColumn_id}
+                        'fieldsLimit': $scope.fieldsLimit,
+                        'columnsLimit' : $scope.columnsLimit,
+                        'fields': $scope.selected,
+                        'conditionColumn_id': $scope.conditionColumn_id}
                     }
                 })
                 .success(function(data, status, headers, config) {
@@ -107,81 +113,24 @@
             }
         }
 
-        $scope.getApplicableOptions();
-
-
         $scope.showQuestion = function(ev){
             $mdDialog.show({
-
-                controller: function($scope, $mdDialog){
-                    $scope.questions = {};
-                    $scope.selected_questions = [];
-
-                    $scope.getApplicableOptions = function() {
-                        $http({method: 'POST', url: 'getApplicableOptions', data:{}})
-                        .success(function(data, status, headers, config) {
-                            angular.extend($scope.questions, data.options.questions);
-                        })
-                        .error(function(e){
-                            console.log(e);
-                        });
-                    }
-                    $scope.getApplicableOptions();
-
-                    $scope.toggle = function (item, list) {
-                        item.selected = !item.selected;
-                        var idx = list.indexOf(item.id);
-                        if(idx>-1){
-                            list.splice(idx, 1);
-                        }else {
-                            list.push(item.id);
-                        }
-                        console.log(list);
-                    };
-
+                controller: function($scope, $mdDialog, fieldsFactory){
+                    $scope.questions = fieldsFactory.questions;
+                    $scope.selected = fieldsFactory.selected;
+                    console.log($scope.questions);
                     $scope.selectAllPage = function(page){
-                        var length = 0;
-                        angular.forEach(page, function(value, key){
-                            if(value.selected){
-                                length++;
+                        var selected = fieldsFactory.selected;
+                        angular.forEach(page, function(question){
+                            if(selected.indexOf(question.id) == -1) {
+                                question.selected = true;
+                                selected.push(question.id);
                             }
                         })
-
-                        if(length == page.length){
-                            angular.forEach(page, function(value, key){
-                                value.selected = false;
-                            })
-                        }else if(length != page.length){
-                            angular.forEach(page, function(value, key){
-                                value.selected = true;
-                            })
-                        }
                     }
 
-                    $scope.save = function(){
-
-                        $mdDialog.cancel();
-                    }
-                    $scope.cancel = function() {
-                        $mdDialog.cancel();
-                    };
-
-                    $scope.previousPage = function(page){
-                        var questions = [];
-                        angular.forEach($scope.questions, function(value,key){
-                            questions.push(key);
-                        })
-                        var index = (questions.indexOf(page)-1) > -1 ? questions.indexOf(page)-1 : 0;
-                        $scope.pages = questions[index];
-                    }
-
-                    $scope.nextPage = function(page){
-                        var questions = [];
-                        angular.forEach($scope.questions, function(value,key){
-                            questions.push(key);
-                        })
-                        var index = (questions.indexOf(page)+1) < questions.length-1 ? questions.indexOf(page)+1 : questions.length-1;
-                        $scope.pages = questions[index];
+                    $scope.toggle = function(item) {
+                        fieldsFactory.toggle(item);
                     }
                 },
                 template: `
@@ -197,31 +146,21 @@
                             <div class="md-dialog-content">
                                 <h2>{{book.title}}</h2>
                                 <div>
-                                    <button class="ui small blue button" ng-click="selectAllPage(questions[pages])">全選此頁</button>
-                                    <md-button ng-click="previousPage(pages)">前一頁</md-button>
-                                    <md-button ng-click="nextPage(pages)">後一頁</md-button>
-                                    <md-select ng-model="pages">
+                                    <button class="ui small blue button" ng-click="selectAllPage(questions[page])">全選此頁</button>
+                                    <md-select ng-model="page">
                                         <md-option ng-repeat="(key,page) in questions" ng-value="key" >{{$index+1}}</md-option>
                                     </md-select>
-                                    <applicable-column ng-if="pages" pages="questions[pages]"></applicable-column>
+                                    <applicable-column page="questions[page]"></applicable-column>
                                 </div>
                             </div>
                         </md-dialog-content>
-
-                        <md-dialog-actions layout="row">
-                            <md-button ng-click="save()">新增</md-button>
-                            <md-button ng-click="cancel()">取消</md-button>
-                        </md-dialog-actions>
                   </form>
                 </md-dialog>
                 `,
                 parent: angular.element(document.body),
                 targetEvent: ev,
                 clickOutsideToClose:true,
-                fullscreen: true,
-                locals: {
-
-                },
+                fullscreen: true
             })
         }
 
@@ -233,18 +172,14 @@
             replace: true,
             transclude: false,
             scope: {
-                pages: '=',
+                page: '=',
             },
             template: `
-                <div ng-repeat="question_column in pages">
-                    {{question_column.title}}
-                    <md-checkbox ng-click="toggle(question_column, selected_questions)" ng-checked="question_column.selected" aria-label=" ">
+                <div ng-repeat="question in page">
+                    {{question.title}}
+                    <md-checkbox ng-click="toggle(question)" ng-checked="question.selected" aria-label=" ">
                 </div>
-
-            `,
-            controller: function($scope){
-
-            }
+            `
         }
     })
 
